8237ec70e04d2eb864eb6da763929f244450064f
casey cole <casey@capsinc.com>
2021-06-08 15:40:53 -0400

    now the temp variable are now dynamically set

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,7 +2,7 @@ import 'package:clima/services/location.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/networking.dart';
-
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -26,15 +26,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
     latitude = location.latitude;
     longitude = location.longitude;
     NetworkHelper networkHelper = NetworkHelper(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=imperial'));
     var weatherData = await networkHelper.getData();
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen();
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
     }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
+    );
   }
 }
