4483a83ec7a1138f5b7fbad28891479827671d02
casey cole <casey@capsinc.com>
2021-06-12 09:23:20 -0400

    fixed null error when unable to get location data

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -24,6 +24,13 @@ class _LocationScreenState extends State<LocationScreen> {
 
   void updateUI(dynamic weatherData) {
     setState(() {
+      if (weatherData == null) {
+        temperature = 0;
+        weatherIcon = "Error";
+        weatherMessage = "Unable to get weather data";
+        cityName = '';
+        return;
+      }
       temperature = weatherData['main']['temp'].toInt();
       var condition = weatherData['weather'][0]['id'];
       weatherIcon = weather.getWeatherIcon(condition);
