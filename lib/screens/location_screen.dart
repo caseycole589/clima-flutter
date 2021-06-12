d00ba78d0a7ebfadda6b394fa8c88a0431db2d6e
casey cole <casey@capsinc.com>
2021-06-12 10:05:06 -0400

    added input for getting weather data for custom city location

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -2,6 +2,8 @@ import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
+import 'city_screen.dart';
+
 class LocationScreen extends StatefulWidget {
   LocationScreen({this.locationWeather});
 
@@ -71,7 +73,16 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -20,3 +20,19 @@ const kConditionTextStyle = TextStyle(
 );
 
 const apiKey = 'c36e199b215ba9017d1122958fad594a';
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(Icons.location_city, color: Colors.white),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
